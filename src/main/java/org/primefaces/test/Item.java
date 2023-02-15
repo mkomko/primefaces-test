27f10a3f348c47ec851f728b9fb603f854088e0f
mko <mkomko@gmail.com>
2023-02-15 12:27:25 +0100

    Include more context in reproducer to provide meaningful example

--- src/main/java/org/primefaces/test/Item.java
+++ src/main/java/org/primefaces/test/Item.java
@@ -26,4 +26,9 @@ public class Item implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "org.primefaces.test.Item[id=" + id + "]";
+    }
 }
