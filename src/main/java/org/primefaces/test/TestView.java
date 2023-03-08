47d29741c384348275c53097aa9be93f5dfbb020
mko <mkomko@gmail.com>
2023-03-08 09:10:49 +0100

    NullPointerException in FileUpload component

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,27 +1,33 @@
 package org.primefaces.test;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
+
 import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 
 import lombok.Data;
+import org.primefaces.event.FileUploadEvent;
 
 @Data
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
+
     private String string;
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
-    
-    @PostConstruct  
+
+    @PostConstruct
     public void init() {
         string = "Welcome to PrimeFaces!!!";
     }
 
+    public void onFileUpload(FileUploadEvent event) {
+        System.out.println("File uploaded: " + event.getFile().getFileName());
+    }
 }
